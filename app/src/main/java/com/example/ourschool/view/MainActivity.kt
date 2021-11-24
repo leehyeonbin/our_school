efca4b739fb81646a7c399983e3d30b745e77f58
이현빈 <82383983+leehyeonbin@users.noreply.github.com>
2021-11-24 09:30:09 +0900

    [FIX] 메뉴 ID 변경

--- app/src/main/java/com/example/ourschool/view/MainActivity.kt
+++ app/src/main/java/com/example/ourschool/view/MainActivity.kt
@@ -6,11 +6,10 @@ import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
 import com.example.ourschool.R
 import com.example.ourschool.databinding.ActivityMainBinding
-import com.example.ourschool.view.fragment.HomeFragment
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var mBinding : ActivityMainBinding
+    private lateinit var mBinding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,14 +17,13 @@ class MainActivity : AppCompatActivity() {
         setContentView(mBinding.root)
 
 
-
-        initNavigationBar()
-    }
-
-    fun initNavigationBar() {
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragment_view) as NavHostFragment
         val navController = navHostFragment.navController
 
         NavigationUI.setupWithNavController(mBinding.bottomNavView, navController)
+
+
     }
+
+
 }
