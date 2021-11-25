bd38c4939477c1f5aeacda4945e94ff879d535ad
이현빈 <82383983+leehyeonbin@users.noreply.github.com>
2021-11-25 22:47:46 +0900

    시간

--- app/src/main/java/com/example/ourschool/view/MainActivity.kt
+++ app/src/main/java/com/example/ourschool/view/MainActivity.kt
@@ -2,20 +2,25 @@ package com.example.ourschool.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.lifecycle.ViewModel
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
 import com.example.ourschool.R
 import com.example.ourschool.databinding.ActivityMainBinding
+import com.example.ourschool.viewmodel.MainViewModel
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var mBinding: ActivityMainBinding
+    private val viewModel by viewModels<MainViewModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         mBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(mBinding.root)
 
+        viewModel.getMeals()
 
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragment_view) as NavHostFragment
         val navController = navHostFragment.navController
--- app/src/main/java/com/example/ourschool/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/example/ourschool/viewmodel/MainViewModel.kt
@@ -1,7 +1,15 @@
 package com.example.ourschool.viewmodel
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
+import com.example.ourschool.model.RetrofitBuild
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+
 
 class MainViewModel : ViewModel() {
-
+    fun getMeals() {
+        val date = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd"))
+        Log.d("TAG", date)
+    }
 }
